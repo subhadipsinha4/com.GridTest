19b2631b6dd16fdee30416e6da90e6630916d726
subhadip sinha <subhadip.sinha@unbxd.com>
2021-05-23 21:43:44 +0530

    push new code

---
+++ src/test/java/openBrowser.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class openBrowser {
+
+    public static WebDriver driver;
+
+    @Test
+    public void setUp()
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\com.GridTest\\driver\\chromedriver.exe");
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get( "https://www.miniaturemarket.com/" );
+        System.out.println(driver.getTitle());
+        driver.close();
+    }
+}
