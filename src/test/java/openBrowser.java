0be75a36b7de4a7ce07aa071e0ec80f69d49832c
subhadip sinha <subhadip.sinha@unbxd.com>
2021-05-30 23:40:46 +0530

    updated 30 may 21

--- src/test/java/openBrowser.java
+++ src/test/java/openBrowser.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -15,24 +16,25 @@ public class openBrowser {
     public static WebDriver driver;
     public static DesiredCapabilities cap;
     //public static ChromeOptions options;
-    public static FirefoxOptions options;
+    //public static FirefoxOptions options;
+    public static EdgeOptions options;
+
     public openBrowser(){
-//        cap=new DesiredCapabilities();
-//        //options=new ChromeOptions();
-//        options=new FirefoxOptions();
+        cap=new DesiredCapabilities();
+        //options=new ChromeOptions();
+        //options=new FirefoxOptions();
+        options=new EdgeOptions();
     }
 
     public void setUp() throws MalformedURLException {
-//        cap.setBrowserName( "firefox" );
-//        cap.setPlatform( Platform.WINDOWS );
-//        options.merge( cap );
-//        String hubUrl="http://192.168.0.104:4444/wd/hub";
-        System.setProperty("webdriver.chrome.driver","C:\\com.GridTest\\driver\\chromedriver.exe");
-        driver=new ChromeDriver();
-        //driver= new RemoteWebDriver( new URL(hubUrl),options );
+        cap.setBrowserName( "MicrosoftEdge" );
+        cap.setPlatform( Platform.WINDOWS );
+        options.merge( cap );
+        String hubUrl="http://192.168.0.104:4444/wd/hub";
+        driver= new RemoteWebDriver( new URL(hubUrl),options );
         driver.manage().window().maximize();
         driver.get( "https://www.miniaturemarket.com/" );
-        //Maven Dependency Update Trigger
+
 
     }
 
